fc44e904ba7e64d020a901b8395ada94a7012488
WildMix <luigigesualdo21022000@gmail.com>
2022-07-19 17:27:06 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { BeerListComponent } from '../beer-list/beer-list.component';
 
 const routes: Route[] = [
